419b9df80bbab058023dfacc4944a54dacbf6a98
Gautamkumardev <36812125+Gautamkumardev@users.noreply.github.com>
2020-04-10 16:49:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggle() {
+
+  var nav = document.getElementById('nav');
+  var toggler = document.getElementById('toggle');
+
+  toggler.classList.toggle("change");
+
+  if (nav.style.maxHeight) {
+    nav.style.maxHeight = null;
+    nav.style.overflow = "hidden";
+
+  } else {
+    nav.style.maxHeight = nav.scrollHeight + "px";
+    setTimeout(function() {
+      nav.style.overflow = "visible";
+    }, 200);
+
+  }
+
+}
